7e9e553ed544be9dbdb7c56713b6ebbd5c25c48c
samuel <samuelp@uci.edu>
2020-01-16 11:34:59 -0800

    Added GridSearch calls in train_classifier and corresponding settings in global_settings

--- hw1/classify.py
+++ hw1/classify.py
@@ -1,25 +1,33 @@
 #!/bin/python
 import global_settings as g
+
+
 def train_classifier(X, y):
-	"""Train a classifier using the given training data.
+    """Train a classifier using the given training data.
 
 	Trains a logistic regression on the input data with default parameters.
 	"""
-	from sklearn.linear_model import LogisticRegression
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.model_selection import GridSearchCV
+    if g.scaling:
+        from sklearn import preprocessing
+        X = preprocessing.scale(X, with_mean=False)
+    clf = GridSearchCV(
+        LogisticRegression(), g.gridSearchParams, n_jobs=-1
+    )
+    clf.fit(X, y)
+    print(clf.best_params_)
+    cls = LogisticRegression(**clf.best_params_)
+    cls.fit(X, y)
+    return cls
 
-	if g.scaling:
-		from sklearn import preprocessing
-		X = preprocessing.scale(X, with_mean=False)
-	cls = LogisticRegression(C = g.C, max_iter=g.max_iter)
-	cls.fit(X, y)
-	return cls
 
 def evaluate(X, yt, cls):
-	"""Evaluated a classifier on the given labeled data using accuracy."""
-	from sklearn import metrics, preprocessing
+    """Evaluated a classifier on the given labeled data using accuracy."""
+    from sklearn import metrics, preprocessing
 
-	if g.scaling:
-		X = preprocessing.scale(X, with_mean=False)
-	yp = cls.predict(X)
-	acc = metrics.accuracy_score(yt, yp)
-	print("  Accuracy", acc)
+    if g.scaling:
+        X = preprocessing.scale(X, with_mean=False)
+    yp = cls.predict(X)
+    acc = metrics.accuracy_score(yt, yp)
+    print("  Accuracy", acc)
--- hw1/global_settings.py
+++ hw1/global_settings.py
@@ -1,9 +1,16 @@
 #!/bin/python
-C = 5
+import numpy
+C = numpy.linspace(0.5, 10, 20)
 max_iter = 1000
 
+solver = ['sag', 'liblinear', 'lbfgs']
 scaling = False
 tfidf = True
 use_idf = True
 ngram_max = 1
 stop_vocab = None
+
+gridSearchParams = {'C': C,
+                    'solver': solver,
+                    'max_iter': [max_iter],
+                    'fit_intercept': [True, False]}
