82bc64bf99aea38e77e76373ffca8accfb946d7f
samuel <samuelp@uci.edu>
2020-02-06 22:24:20 -0800

    Fixed a bug in cond_logprob that was calculating log base 10 instead of log base 10. Other minor bug fixes to get cond_logprob to run error-free.

--- hw2/lm.py
+++ hw2/lm.py
@@ -104,157 +104,168 @@ class Unigram(LangModel):
     def vocab(self):
         return self.model.keys()
 
-    class Ngram(LangModel):
-        def __init__(self, unk_prob=0.0001, n=3):
-            self.model = dict()
-            from collections import defaultdict
-            self.totals = defaultdict(int)  # total count for every word
-            self.lunk_prob = log(unk_prob, 2)
-            # self.contexts = defaultdict(int)  # total count for every context (not including word)
-            self.context_sum = 'CONTEXT_SUM'
-            if n > 0:
-                self.n = n
+
+class Ngram(LangModel):
+    def __init__(self, unk_prob=0.0001, n=3, smooth=1):
+        self.model = dict()
+        from collections import defaultdict
+        self.totals = defaultdict(int)  # total count for every word
+        self.lunk_prob = log(unk_prob, 2)
+        self.smooth = smooth
+        # self.contexts = defaultdict(int)  # total count for every context (not including word)
+        self.context_sum = 'CONTEXT_SUM'
+        self.sos = 'START_OF_SENTENCE'
+        self.unks = set()
+        if n > 0:
+            self.n = n
+        else:
+            print('N must be 1 or greater')
+            raise ValueError()
+
+    def fit_corpus(self, corpus):
+        """Learn the language model for the whole corpus.
+
+        The corpus consists of a list of sentences."""
+        self.__get_totals(corpus)
+        self.unks = self.__unk_parse()
+        for s in corpus:
+            self.fit_sentence(s)
+        # self.norm()
+
+    def inc_word(self, context):
+        """Given a list containing a word and its previous N-1 words, add these to the model and update the count.
+        Example: ['Sam', 'I', 'am'] where the word is 'am', using a trigram model"""
+
+        assert (len(context) == self.n)  # first check that the list is the correct length
+        context_sum = self.context_sum
+
+        curr_level = self.model
+        for i in range(self.n):
+            w = context[i]
+            if i < self.n - 1:  # if not at actual word (still at context word)
+                if w not in curr_level:
+                    # add entry if it didn't exist before
+                    curr_level[w] = dict()
+                curr_level = curr_level[w]
             else:
-                print('N must be 1 or greater')
-                raise ValueError()
-
-        def fit_corpus(self, corpus):
-            """Learn the language model for the whole corpus.
-
-            The corpus consists of a list of sentences."""
-            self.__get_totals(corpus)
-            for s in corpus:
-                self.fit_sentence(s)
-            # self.norm()
-
-        def inc_word(self, context):
-            """Given a list containing a word and its previous N-1 words, add these to the model and update the count.
-            Example: ['Sam', 'I', 'am'] where the word is 'am', using a trigram model"""
-
-            assert (len(context) == self.n)  # first check that the list is the correct length
-            context_sum = self.context_sum
-
-            curr_level = self.model
-            for i in range(self.n):
-                w = context[i]
-                if i < self.n - 1:  # if not at actual word (still at context word)
-                    if w not in curr_level:
-                        # add entry if it didn't exist before
-                        curr_level[w] = dict()
-                    curr_level = curr_level[w]
+                if w in curr_level:
+                    curr_level[w] += 1.0
                 else:
-                    if w in curr_level:
-                        curr_level[w] += 1.0
-                    else:
-                        curr_level[w] = 1.0
+                    curr_level[w] = 1.0
 
-            # increment sum of all words given this context
-            if context_sum in curr_level:
-                curr_level[context_sum] += 1.0
-            else:
-                curr_level[context_sum] = 1.0
+        # increment sum of all words given this context
+        if context_sum in curr_level:
+            curr_level[context_sum] += 1.0
+        else:
+            curr_level[context_sum] = 1.0
 
-            # self.contexts[context[:-1]] += 1
+        # self.contexts[context[:-1]] += 1
 
-        def __get_totals(self, corpus):
-            for s in corpus:
-                for w in s:
-                    self.totals[w] += 1
-                self.totals['END_OF_SENTENCE'] += 1
+    def __get_totals(self, corpus):
+        for s in corpus:
+            for w in s:
+                self.totals[w] += 1
+            self.totals['END_OF_SENTENCE'] += 1
 
-        def fit_sentence(self, sentence):
-            """Add start of sentence markers and place words and their contexts into model. self.totals must be
-            computed already """
-            unks = self.__unk_parse()  # get set of unknowns first
-            sos = 'START_OF_SENTENCE'
-            unk = 'UNK'
+    def fit_sentence(self, sentence):
+        """Add start of sentence markers and place words and their contexts into model. self.totals must be
+        computed already """
+        unks = self.unks  # get set of unknowns first
+        sos = self.sos
+        unk = 'UNK'
+        context = []
+        for w_index in range(len(sentence)):
             context = []
-            for w_index in range(len(sentence)):
-                context = []
-                for sos_char in range(self.n - 1 - w_index):
-                    context.append(sos)  # add appropriate number of start of sentence tokens
-                curr_word = ""
-                for offset in range(-min(w_index, self.n - 1), 1, 1):
-                    curr_word = sentence[w_index + offset]
-                    if curr_word in unks:
-                        curr_word = unk
-                    context.append(curr_word)  # add the remaining context to the list
-                if curr_word in unks:  # if the word (not context) is unk, increase its count
-                    self.totals[unk] += 1
-                self.inc_word(context)
-
-            context.append('END_OF_SENTENCE')
-            context.pop(0)
+            for sos_char in range(self.n - 1 - w_index):
+                context.append(sos)  # add appropriate number of start of sentence tokens
+            curr_word = ""
+            for offset in range(-min(w_index, self.n - 1), 1, 1):
+                curr_word = sentence[w_index + offset]
+                if curr_word in unks:
+                    curr_word = unk
+                context.append(curr_word)  # add the remaining context to the list
+            if curr_word in unks:  # if the word (not context) is unk, increase its count
+                self.totals[unk] += 1
             self.inc_word(context)
 
-        def __unk_parse(self):
-            """Returns a list of unknown words"""
-            unknowns = set()
-            ltotal = log(sum(self.totals.values()), 2)  # total num of all words
-            for word, count in self.totals.items():  # get list of unknown words
-                if log(count, 2) - ltotal < self.lunk_prob:
-                    unknowns.add(word)
+        context.append('END_OF_SENTENCE')
+        context.pop(0)
+        self.inc_word(context)
 
-            return unknowns
+    def __unk_parse(self):
+        """Returns a list of unknown words"""
+        unknowns = set()
+        ltotal = log(sum(self.totals.values()), 2)  # total num of all words
+        for word, count in self.totals.items():  # get list of unknown words
+            if log(count, 2) - ltotal < self.lunk_prob:
+                unknowns.add(word)
 
-        def norm(self):
-            """Normalize and convert to log2-probs, including smoothing."""
-            for word in self.model:
-                ldenom = log(self.totals[word] + self.smooth * len(self.model), 2)
-                self.__norm_recurse(self.model[word], ldenom)
+        return unknowns
 
-        def __norm_recurse(self, d, ltotal):
-            for k, v in d.iteritems():
-                if isinstance(v, d):
-                    self.__norm_recurse(v, ltotal)
-                else:
-                    d[k] = log(d[k] + self.smooth, 2) - ltotal
+    def norm(self):
+        """Normalize and convert to log2-probs, including smoothing."""
+        for word in self.model:
+            ldenom = log(self.totals[word] + self.smooth * len(self.model), 2)
+            self.__norm_recurse(self.model[word], ldenom)
 
-        def cond_logprob(self, word, previous, numOOV):
-            """Returns the conditional log probability given a word and its N-gram context.
-            The context is a list of the N-1 words before the given word."""
-            assert len(previous) == self.n - 1
-            V = len(self.vocab() - 1)
-            lunk_factor = 0  # set to numOOV if word is an unknown
-            if word not in self.model:
-                word = 'UNK'
-                lunk_factor = log(numOOV, 2)
-
-            num_in_context = self.__context_checker(previous)
-            if num_in_context == -1:  # if context not found, return log(1/V)
-                return -log(len(V-1), 2) - lunk_factor
-
-            #  go to dictionary containing word (n-1th level)
-            curr_level = self.model
-            for i in range(0, len(previous)):
-                curr_level = curr_level[previous[i]]
-            #  check if word hasn't appeared in this context
-            if word not in curr_level:
-                return log(self.smooth, 2) - log(self.smooth*V + num_in_context) - lunk_factor
+    def __norm_recurse(self, d, ltotal):
+        for k, v in d.iteritems():
+            if isinstance(v, d):
+                self.__norm_recurse(v, ltotal)
             else:
-                cond_count = curr_level[word]
-                return log(self.smooth + cond_count, 2) - log(self.smooth*V + num_in_context) - lunk_factor
+                d[k] = log(d[k] + self.smooth, 2) - ltotal
 
-        def __context_checker(self, previous):
-            """Return -1 if context doesn't exist.
-            Else return the total number of words in this context"""
-            curr_level = self.model
-            for x in previous:
-                if x in curr_level:
-                    curr_level = curr_level[x]
-                else:
-                    return -1
-            return curr_level[self.context_sum]
+    def cond_logprob(self, word, previous, numOOV):
+        """Returns the conditional log probability given a word and its N-gram context.
+        The context is all the words in a sentence before the given word."""
+        if len(previous) < self.n - 1:
+            num_sos = self.n - 1 - len(previous)
+            previous = [self.sos]*num_sos + previous
+        else:
+            previous = previous[len(previous) - self.n + 1: len(previous)]
+        assert len(previous) == self.n - 1
 
-        def vocab(self):
-            return self.model.keys()
+        V = len(self.vocab())
+        lunk_factor = 0  # set to numOOV if word is an unknown
+        if word not in self.model:
+            word = 'UNK'
+            lunk_factor = log(numOOV, 2)
 
-        def perplexity(self, corpus):
-            """Computes the perplexity of the corpus by the model.
+        num_in_context = self.__context_checker(previous)
+        if num_in_context == -1:  # if context not found, return log(1/V)
+            return -log(V, 2) - lunk_factor
 
-            Assumes the model uses an EOS symbol at the end of each sentence.
-            """
-            vocab_set = set(self.vocab())
-            words_set = set([w for s in corpus for w in s])
-            numOOV = len(words_set - (vocab_set - 1))  # -1 b/c end of sentence is in model
-            return pow(2.0, self.entropy(corpus, numOOV))
+        #  go to dictionary containing word (n-1th level)
+        curr_level = self.model
+        for i in range(0, len(previous)):
+            curr_level = curr_level[previous[i]]
+        #  check if word hasn't appeared in this context
+        if word not in curr_level:
+            return log(self.smooth, 2) - log(self.smooth*V + num_in_context, 2) - lunk_factor
+        else:
+            cond_count = curr_level[word]
+            return log(self.smooth + cond_count, 2) - log(self.smooth*V + num_in_context, 2) - lunk_factor
+
+    def __context_checker(self, previous):
+        """Return -1 if context doesn't exist.
+        Else return the total number of words given this context"""
+        curr_level = self.model
+        for x in previous:
+            if x in curr_level:
+                curr_level = curr_level[x]
+            else:
+                return -1
+        return curr_level[self.context_sum]
+
+    def vocab(self):
+        return self.model.keys()
+
+    def perplexity(self, corpus):
+        """Computes the perplexity of the corpus by the model.
+
+        Assumes the model uses an EOS symbol at the end of each sentence.
+        """
+        vocab_set = set(self.vocab())
+        words_set = set([w for s in corpus for w in s])
+        numOOV = len(words_set - vocab_set)
+        return pow(2.0, self.entropy(corpus, numOOV))
