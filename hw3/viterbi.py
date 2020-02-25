7e18ed57969ec464f6fdf109e144374d3afe51fc
SamuelPhang <40727683+SamuelPhang@users.noreply.github.com>
2020-02-25 15:22:03 -0800

    Implemented viterbi decoding score calculation

--- hw3/viterbi.py
+++ hw3/viterbi.py
@@ -8,7 +8,7 @@ def run_viterbi(emission_scores, trans_scores, start_scores, end_scores):
     L - number of labels
 
     As an input, you are given:
-    - Emission scores, as an NxL array
+    - Emission scores, as an NxL array - score[token][label]
     - Transition scores (Yp -> Yc), as an LxL array
     - Start transition scores (S -> Y), as an Lx1 array
     - End transition scores (Y -> E), as an Lx1 array
@@ -24,9 +24,34 @@ def run_viterbi(emission_scores, trans_scores, start_scores, end_scores):
     assert emission_scores.shape[1] == L
     N = emission_scores.shape[0]
 
-    y = []
+    y = [] # score table
+    sequence = [] # final sequence
+    # init table, N rows and L columns
     for i in range(N):
-        # stupid sequence
-        y.append(i % L)
+        y.append([0]*L)
+
+    for i in range(N):
+        for y_i in range(L):
+            max_score = float("-inf")
+            for y_prev in range(L):
+                score = 0
+                if i == 0: # if first token, use start transition
+                    # note, previous score is zero
+                    score = emission_scores[i][y_i] + start_scores[y_i]
+                else: # else lookup transition score in table
+                    score = emission_scores[i][y_i] + trans_scores[y_prev][y_i] + y[i-1][y_prev]
+                
+                max_score = max(score, max_score)
+            y[i][y_i] = max_score 
+
+    final_score = float("-inf")
+    for y_end in range(L): # consider end transition scores, assume zero emission for eos
+        # y_end is the label of the last token
+        score = end_scores[y_end] + y[N-1][y_end]
+        final_score = max(score, final_score)
+
+    # TODO: get sequence
+    sequence = [0]*N
+
     # score set to 0
-    return (0.0, y)
+    return (final_score, sequence)
